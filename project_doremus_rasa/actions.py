90cdee4dbcd31db01db7c1993ab9affee779b4ab
danielhomsa <danielha23@outlook.com>
2019-04-08 20:36:34 +0200

    Add files via upload

---
+++ project_doremus_rasa/actions.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+import requests
+import json
+from rasa_core_sdk import Action
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+logger = logging.getLogger(__name__)
+
+
+class ActionJoke(Action):
+    def name(self):
+        # define the name of the action which can then be included in training stories
+        return "action_joke"
+
+    def run(self, dispatcher, tracker, domain):
+        # what your action should do
+        request = json.loads(requests.get('https://api.chucknorris.io/jokes/random').text)  # make an api call
+        joke = request['value']  # extract a joke from returned json response
+        dispatcher.utter_message(joke)  # send the message back to the user
+        return []
+
+class ActionWorksBy(Action):
+	def name(self):
+		return "action_works_by"
+
+	def run(self, dispatcher, tracker, domain):
+		number = tracker.get_slot('number')
+		artist = tracker.get_slot('doremus-artist')
+		# sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+		# sparql.setQuery("""PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?label WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }""")
+		# sparql.setReturnFormat(JSON)
+		# results = sparql.query().convert()
+		# new = results["results"]["bindings"]
+		# res = new[1]['label']['value']
+		# dispatcher.utter_message(res)
+		# dispatcher.utter_message(results["results"]["bindings"])
+		# return []
+		# for result in results["results"]["bindings"]:
+		# 	print('%s: %s' % (result["label"]["xml:lang"], result["label"]["value"]))
+		# return []
+		sparql = SPARQLWrapper("http://data.doremus.org/sparql")
+		sparql.setQuery("""PREFIX mus: <http://data.doremus.org/ontology#> 
+							PREFIX ecrm: <http://erlangen-crm.org/current/>
+    						PREFIX efrbroo: <http://erlangen-crm.org/efrbroo/>
+    						PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+    						SELECT DISTINCT ?expression SAMPLE(?title) as ?title
+    						WHERE {
+    						?expression a efrbroo:F22_Self-Contained_Expression ;
+              				rdfs:label ?title .
+      						?expCreation efrbroo:R17_created ?expression ;
+              				ecrm:P9_consists_of / ecrm:P14_carried_out_by ?composer .
+      						?composer foaf:name \"""" +artist.capitalize()+ """\"
+    						} ORDER BY rand() LIMIT """ + str(number))
+		sparql.setReturnFormat(JSON)
+		results = sparql.query().convert()
+		print(results)
+		print(number)
+		print(artist.capitalize())
+		return []
---
+++ project_doremus_rasa/data/stories.md
@@ -0,0 +1,45 @@
+## story_greet <!--- The name of the story. It is not mandatory, but useful for debugging. --> 
+* greet <!--- User input expressed as intent. In this case it represents users message 'Hello'. --> 
+ - utter_name <!--- The response of the chatbot expressed as an action. In this case it represents chatbot's response 'Hello, how can I help?' --> 
+ 
+## story_goodbye
+* goodbye
+ - utter_goodbye
+
+## story_thanks
+* thanks
+ - utter_thanks
+ 
+## story_laugh
+* laugh
+ - utter_laugh
+
+## story_name
+* name{"name":"Sam"}
+ - utter_greet
+ 
+
+## story_joke_01
+* joke
+ - action_joke
+ 
+## story_joke_02
+* greet
+ - utter_name
+* name{"name":"Lucy"} <!--- User response with an entity. In this case it represents user message 'My name is Lucy.' --> 
+ - utter_greet
+* joke
+ - action_joke
+* thanks
+ - utter_thanks
+* goodbye
+ - utter_goodbye 
+
+## story_works_by
+* works_by{"number":"1", "doremus-artist":"Vivaldi"}
+ - utter_works_by
+ - action_works_by
+
+## story_discover_artist
+* discover_artist{"doremus-artist":"Bach"}
+ - utter_discover_artist
